23620aa55d862b6d385870f76f710067188d8e18
Seasa_Huang <ericet1234@gmail.com>
2018-08-23 11:44:48 +0800

    temp

--- pytorch/expand_test.py
+++ pytorch/expand_test.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 
-a = torch.range(1,5)
-z = torch.zeros(5,5)
-
+a = torch.zeros([2,5])
+b = torch.ones([2,1])
 print(a)
-print(z)
-a = a.expand_as(z)
-print(a)
+print(b)
+b = b.expand_as(a)
+print(b)
